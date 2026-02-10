8a9276036f85b90bdeca263a2ea3e7f20f43096c
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2026-02-10 14:49:51 +0200

    modify test script for mpi

--- hpc_ex01_code/multistart/test.c
+++ hpc_ex01_code/multistart/test.c
@@ -1,21 +1,41 @@
+// #include <stdio.h>
+// #include <omp.h>
+// #include <unistd.h>
+
+// int main(int argc,char *argv[]) {
+
+//     #pragma omp parallel
+//     {
+//         #pragma omp single
+//         {
+//             for (int i=0; i<20; i++)
+//             {
+//                 #pragma omp task
+//                 sleep(2);
+//             }
+//             printf("Hello from me: %i\n", omp_get_thread_num());
+//             #pragma omp taskwait
+//         }
+//     }
+//     return 0;
+// }
+
+
 #include <stdio.h>
-#include <omp.h>
+// #include <omp.h>
 #include <unistd.h>
+#include <mpi/mpi.h>
 
 int main(int argc,char *argv[]) {
 
-    #pragma omp parallel
-    {
-        #pragma omp single
-        {
-            for (int i=0; i<20; i++)
-            {
-                #pragma omp task
-                sleep(2);
-            }
-            printf("Hello from me: %i\n", omp_get_thread_num());
-            #pragma omp taskwait
-        }
-    }
+    MPI_Init(&argc, &argv);
+    int rank;
+    int size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("Hello from me: %i\n", rank);
+
+    MPI_Finalize();
     return 0;
 }
