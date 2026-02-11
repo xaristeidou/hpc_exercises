126d1a3eace9e373fa5670bf62d6efe1af25dee9
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2026-02-11 10:50:05 +0200

    still testing...

--- hpc_ex01_code/multistart/multistart_mds_mpi.c
+++ hpc_ex01_code/multistart/multistart_mds_mpi.c
@@ -13,6 +13,13 @@
 extern void mds(double *startpoint, double *endpoint, int n, double *val, double eps, int maxfevals, int maxiter,
          double mu, double theta, double delta, int *ni, int *nf, double *xl, double *xr, int *term);
 
+struct Info {
+	int best_trial;
+	int best_nt;
+	int best_nf;
+	int best_fx;
+	double best_pt[MAXVARS];
+};
 
 /* global variables */
 unsigned long funevals = 0;
@@ -98,6 +105,7 @@ int main(int argc, char *argv[])
 	mds(startpt, endpt, nvars, &fx, eps, maxfevals, maxiter, mu, theta, delta,
 		&nt, &nf, lower, upper, &term);
 
+
 	#if DEBUG
 	printf("\n\n\nMDS %d USED %d ITERATIONS AND %d FUNCTION CALLS, AND RETURNED\n", rank, nt, nf);
 	for (i = 0; i < nvars; i++)
@@ -107,36 +115,49 @@ int main(int argc, char *argv[])
 	#endif
 
 	// TODO: collect all fx from each rank
-	MPI_Reduce(&fx, &best_fx, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
+	// MPI_Reduce(&fx, &best_fx, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
 
 	if (rank==0)
+	{		
+		for (int i=1; i<size; i++)
+		{
+			printf("I am in iteration: %d\n\n", i);
+			MPI_Recv(&fx, 1, MPI_DOUBLE, i, 42, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&nt, 1, MPI_INT, i, 43, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&nf, 1, MPI_INT, i, 44, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&endpt, MAXVARS, MPI_DOUBLE, i, 45, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			
+			/* keep the best solution */
+			if (fx < best_fx)
+			{
+				best_trial = i;
+				best_nt = nt;
+				best_nf = nf;
+				best_fx = fx;
+				for (i = 0; i < nvars; i++)
+					best_pt[i] = endpt[i];
+			}
+		}
+		t1 = get_wtime();
+		printf("\n\nFINAL RESULTS:\n");
+		printf("Elapsed time = %.3lf s\n", t1-t0);
+		printf("Total number of trials = %d\n", ntrials);
+		printf("Total number of function evaluations = %ld\n", funevals);
+		printf("Best result at trial %d used %d iterations, %d function calls and returned\n", best_trial, best_nt, best_nf);
+		for (i = 0; i < nvars; i++) {
+			printf("x[%3d] = %15.7le \n", i, best_pt[i]);
+		}
+		printf("f(x) = %15.7le\n", best_fx);
+	}
+	else
 	{
-		printf("Best fx: %f", best_fx);
+		printf("I am sending data my ID is: %d: ", rank);
+		MPI_Ssend(&fx, 1, MPI_DOUBLE, 0, 42, MPI_COMM_WORLD);
+		MPI_Ssend(&nt, 1, MPI_INT, 0, 43, MPI_COMM_WORLD);
+		MPI_Ssend(&nf, 1, MPI_INT, 0, 44, MPI_COMM_WORLD);
+		MPI_Ssend(&endpt, MAXVARS, MPI_INT, 0, 45, MPI_COMM_WORLD);
 	}
 
-	// if (rank==0)
-	// {
-	// 	/* keep the best solution */
-	// 	if (fx < best_fx) {
-	// 		best_trial = trial;
-	// 		best_nt = nt;
-	// 		best_nf = nf;
-	// 		best_fx = fx;
-	// 		for (i = 0; i < nvars; i++)
-	// 			best_pt[i] = endpt[i];
-	// 	}
-	// 	t1 = get_wtime();
-		
-	// 	printf("\n\nFINAL RESULTS:\n");
-	// 	printf("Elapsed time = %.3lf s\n", t1-t0);
-	// 	printf("Total number of trials = %d\n", ntrials);
-	// 	printf("Total number of function evaluations = %ld\n", funevals);
-	// 	printf("Best result at trial %d used %d iterations, %d function calls and returned\n", best_trial, best_nt, best_nf);
-	// 	for (i = 0; i < nvars; i++) {
-	// 		printf("x[%3d] = %15.7le \n", i, best_pt[i]);
-	// 	}
-	// 	printf("f(x) = %15.7le\n", best_fx);
-	// }
 
 	MPI_Finalize();
 	return 0;
